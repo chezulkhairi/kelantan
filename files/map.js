38b0cb5437571a65715d7b9c11b0f5f67ec18e90
CheZulkhairi <chezulkhairi.abdullah@gmail.com>
2018-10-23 21:14:34 +0100

    Update map.js

--- files/map.js
+++ files/map.js
@@ -335,16 +335,26 @@ function loadGeoJsonString(geoString) {
  */
 function initMap() {
 	
-	var customLayer = L.geoJson(null, {
+	var customLayer1 = L.geoJson(null, {
     	// http://leafletjs.com/reference.html#geojson-style
     		style: function(feature) {
-        return { color: '#f00' };
-    	}
-	});
+        return { color: '#f00', "weight": 1, "opacity": 0.65  }; }});
+	
+	var customLayer2 = L.geoJson(null, {
+    	// http://leafletjs.com/reference.html#geojson-style
+    		style: function(feature) {
+        return { "color": "#ff7800", "weight": 1, "opacity": 0.65 }; }});
+	
+	var customLayer3 = L.geoJson(null, {
+    	// http://leafletjs.com/reference.html#geojson-style
+    		style: function(feature) {
+        return { color: '#fff', "weight": 1, "opacity": 0.65  }; }});
+	
+	
 	// this can be any kind of omnivore layer
 	var MalaysiaFlood2018 = omnivore.kml('MalaysiaFlood2018.kml', null, customLayer)
-	var DelaunayFirstResponderNGO = omnivore.kml('DelaunayFirstResponderNGO.kml', null, customLayer)
-	var DelaunayPusatPemindahan = omnivore.kml('DelaunayPusatPemindahan.kml', null, customLayer)
+	var DelaunayFirstResponderNGO = omnivore.kml('DelaunayFirstResponderNGO.kml', null, customLayer1)
+	var DelaunayPusatPemindahan = omnivore.kml('DelaunayPusatPemindahan.kml', null, customLayer2)
 	var FirstResponderNGO = omnivore.kml('FirstResponderNGO.kml', null, customLayer)
 	var Kobo = omnivore.kml('Kobo.kml', null, customLayer)
 	var KoboPusatPemindahan = omnivore.kml('KoboPusatPemindahan.kml', null, customLayer)
@@ -355,7 +365,7 @@ function initMap() {
 	var accessibilityKelantanS1 = omnivore.kml('accessibilityKelantanS1.kml', null, customLayer)
 	var accessibilityKelantanS4 = omnivore.kml('accessibilityKelantanS4.kml', null, customLayer)
 	var kelantanRekodBanjir2007_sourceCRISP = omnivore.kml('kelantanRekodBanjir2007_sourceCRISP.kml', null, customLayer)
-	var pusatpemindahan = omnivore.kml('pusatpemindahan.kml', null, customLayer)
+	var pusatpemindahan = omnivore.kml('pusatpemindahan.kml', null, customLayer3)
 	
 	var floodmap = L.layerGroup([DelaunayFirstResponderNGO, DelaunayPusatPemindahan, pusatpemindahan]);
 	
